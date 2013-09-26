babca93b7029af74d0fda223e4a638cdc85d9859
Gerardo Barcia Palacios <gerardobarciap@gmail.com>
2013-09-26 13:10:49 +0200

    initial structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angostura::Application.config.secret_key_base = '03e38872105120b40eb258089e026b03f18f3e92d8d3600143a2bebb6f9dfb0810a9f80760fcbee98681f682d3a2d0cfeb9edb4d6a04f3a6f2feb9540e791fbe'
